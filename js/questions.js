ee1bc3c2a3d5f54139c796037b56e588cccf9935
Kingcoopa <69878838+Kingcoopa@users.noreply.github.com>
2021-02-27 14:28:14 -0500

    Q and A Array Push

---
+++ js/questions.js
@@ -0,0 +1,73 @@
+// Array for the questions and answers
+let questions = [
+
+    {
+      number: 1,
+      question: "What does HTML stand for?",
+      answer: "Hyper-Text Markup Language",
+      choices: [
+          "Hi-tech Management Language",
+          "Hyper-Text Markup Language",
+          "Hyper-tool Master Location",
+          "None of the above"
+      ]
+
+  },
+
+    {
+
+      number: 2,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheets",
+      choices: [
+          "Computer Sciense Structure",
+          "Computer System Styling",
+          "CPU Storage System",
+          "Cascading Style Sheets"
+      ]
+
+  },
+
+    {
+
+      number: 3,
+      question: "The condition in an if/else statement is enclosed within ___.",
+      answer: "parentheses",
+      choices: [
+          "Quotes",
+          "Curly Brackets",
+          "parentheses",
+          "Square Brackets"
+      ]
+
+  },
+
+    {
+
+      number: 4,
+      question: "Arrays in JavaScript can be used to store ___.",
+      answer: "all of the above",
+      choices: [
+          "numbers and strings",
+          "other arrays",
+          "booleans",
+          "all of the above"
+      ]
+
+  },
+
+    {
+
+        number: 5,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        choices: [
+            "Styled Quantum Language",
+            "Styled Query Loop",
+            "Standard Query Log",
+            "Structured Query Language"
+      ]
+
+  },
+
+];
